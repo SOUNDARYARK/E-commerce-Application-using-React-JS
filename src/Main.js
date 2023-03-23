f8e4798bb508bad0332b58c926ca5d6ac0a9907f
SOUNDARYA <93415995+SOUNDARYARK@users.noreply.github.com>
2023-03-23 13:21:31 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,7 @@ return(
     <>
     <div className ="hero border-1 pb-3">
         <div className="card bg-dark text-white border-0  mx-3">
-             <img className="card-img img-fluid"  src="./assets/main.jpg" alt="card" height={100}  />
+             <img className="card-img img-fluid"  src="./assets/image.jpg" alt="card" height={100}  />
                  <div className ="card-img-overlay d-flex align-tems-center">
                     <div className="container">
                          <h5 className="card-title fs-1  text fw-lighter">New Season Arrivals</h5>
